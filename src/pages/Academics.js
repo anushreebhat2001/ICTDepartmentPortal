2dc106552a2b48ccdda4b2d0b2f14c768b38df68
anushreebhat2001 <anushree.bhat@learner.manipal.edu>
2022-01-25 00:18:32 +0530

    acad

--- src/pages/Academics.js
+++ src/pages/Academics.js
@@ -2,13 +2,23 @@ import React from "react";
 
 const Academics = () => {
     return (
-        <div>
+        <div id="bg">
             <div className = 'section'>
                 <div className = 'overlay2'>
                 <div className = 'heading2'>
                     I&CT ACADEMICS
                 </div>
                 </div>
+            </div>
+            <div className="cdetails">
+                <h3>Course Details:</h3>
+                <h4><br></br>B-TECH</h4>
+                <p>Computer and Communication Engineering : <a className="course" href="https://manipal.edu/content/dam/manipal/mu/mit/documents/ict_courseoutline/Computer%20and%20Communication_2018.pdf">Course Details</a><br></br><a className="cdet" href="https://manipal.edu/mit/program-list/btech/btech-computer-and-communication-engineering.html">Click here to know more about Computer and Communication Engineering</a></p>
+                <p>Information and Technology Engineering : <a className="course" href="https://manipal.edu/content/dam/manipal/mu/mit/documents/ict_courseoutline/Information%20Technology_2018.pdf">Course Details</a><br></br><a className="cdet" href="https://manipal.edu/mit/program-list/btech/btech-information-technology.html">Click here to know more about Information Technology</a></p>
+                <h4><br></br>M-TECH</h4>
+                <p>Software Engineering : <a className="course" href="https://manipal.edu/content/dam/manipal/mu/mit/documents/ict_courseoutline/M.Tech-Software%20Engg_2019.pdf">Course Details</a><br></br><a className="cdet" href="https://manipal.edu/mit/program-list/mtech/mtech-software-engineering.html">Click here to know more about Software Engineering</a></p>
+                <p>Computer Networking & Engineering : <a className="course" href="https://manipal.edu/content/dam/manipal/mu/mit/documents/ict_courseoutline/M.Tech-Comp.Networking%20and%20Engg._2019.pdf">Course Details</a><br></br><a className="cdet" href="https://manipal.edu/mit/program-list/mtech/mtech-network-engineering.html">Click here to know more about Computer Networking & Engineering</a></p>
+                <p>Machine Intelligence & Decision Science : <a className="course" href="https://manipal.edu/content/dam/manipal/mu/documents/Admissions/adm2021/M%20Tech%20(MIDS)-Course%20Structure.pdf">Course Details</a><br></br><a className="cdet" href="https://manipal.edu/mit/program-list/mtech/MTech-Machine-Intelligence-and-Decision-Science.html">Click here to know more about Machine Intelligence & Decision Science</a></p>
             </div>
         </div>
     );
